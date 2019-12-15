dd3fc378d5cc1fbd302745e0d7f75d5ef3741300
Carmella Winterbauer <carmella@onehopewine.com>
2019-12-14 19:16:36 -0800

    start frontend

--- projects/capstone/starter/app.py
+++ projects/capstone/starter/app.py
@@ -1,14 +1,20 @@
 import os
-from flask import Flask, request, abort, jsonify
+from flask import Flask, request, abort, jsonify, render_template
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 
-def create_app(test_config=None):
-  # create and configure the app
-  app = Flask(__name__)
-  CORS(app)
 
-  return app
+def create_app(test_config=None):
+    # create and configure the app
+    app = Flask(__name__)
+    CORS(app)
+
+    @app.route('/')
+    def index():
+        return render_template('home.html')
+
+    return app
+
 
 APP = create_app()
 
